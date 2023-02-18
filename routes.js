e542b8d79027b4b6a9057c2c6f42fef034385323
sonu mahto <sonumahtoraj121@gmail.com>
2023-02-18 23:35:39 +0530

    Nodemon and debugging skill

--- routes.js
+++ routes.js
@@ -18,18 +18,21 @@ const requestHandler = (req, res) => {
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            //const new_message = message.split('+');
+            fs.writeFileSync('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+
     }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first html page on node.js</title></head>');
-    res.write('<body><h1>I am learning java script</h1></body>')
-    res.write('</html>');
-    res.end();
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<html>');
+    // res.write('<head><title>My first html page on node.js</title></head>');
+    // res.write('<body><h1>I am learning java script</h1></body>')
+    // res.write('</html>');
+    // res.end();
 };
 //module.exports = requestHandler;
 /*module.exports = {
